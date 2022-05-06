15aef12a52aec12807233f417d3965f61b60b9b9
yukisuzuki0314 <bell.yuki19850314@icloud.com>
2022-05-07 08:11:29 +0900

    item、orderモデルのアソシエーションコメントアウト

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,6 @@ class User < ApplicationRecord
           VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
           validates :password, format: { with: VALID_PASSWORD_REGEX, message: ' is invalid. Include both letters and numbers'}
 
-  has_many :items
-  has_many :orders
+  #has_many :items
+  #has_many :orders
 end
