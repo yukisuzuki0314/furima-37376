a375db2fdb615d38aa7287d789da74ba7ba407af
yukisuzuki0314 <bell.yuki19850314@icloud.com>
2022-05-22 02:15:50 +0900

    アイテムモデル単体テストコード

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do
   factory :item do
-    
+    item_name               {Faker::Name.name }
+    item_info               {Faker::Lorem.sentence}
+    category_id             {2}
+    item_status_id          {2}
+    shipping_fee_status_id  {2}
+    prefecture_id           {2}
+    scheduled_delivery_id   {2}
+    price                   {1000}
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
