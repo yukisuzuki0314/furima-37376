0b823f5278f296e444f03d422ed014a3eb3ffe56
yukisuzuki0314 <bell.yuki19850314@icloud.com>
2022-05-22 13:50:00 +0900

    migrateファイル修正

--- db/migrate/20220508095427_create_items.rb
+++ db/migrate/20220508095427_create_items.rb
@@ -1,7 +1,7 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
-      t.string  :item_name,               null: false, default: ""
+      t.string  :item_name,               null: false
       t.text    :item_info,               null: false
       t.integer :category_id,             null: false
       t.integer :item_status_id,          null: false
@@ -9,6 +9,7 @@ class CreateItems < ActiveRecord::Migration[6.0]
       t.integer :prefecture_id,           null: false
       t.integer :scheduled_delivery_id,   null: false
       t.integer :price,                   null: false
+      t.references :user,                 null: false, foreign_key: true
       t.timestamps
     end
   end
