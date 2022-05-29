8aef62be8b891bc7e3bde6e5f45d28ad1334b682
yukisuzuki0314 <bell.yuki19850314@icloud.com>
2022-05-29 23:49:30 +0900

    Rubocop

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -24,12 +24,12 @@ class ItemsController < ApplicationController
   end
 
   def edit
+    @item = Item.find(params[:id])
+    if current_user.id == @item.user_id
       @item = Item.find(params[:id])
-      if current_user.id == @item.user_id
-        @item = Item.find(params[:id])
-      else
-        redirect_to root_path
-      end
+    else
+      redirect_to root_path
+    end
   end
 
   def update
